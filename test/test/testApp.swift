3d5c0a1e158cd49a1e6e963860c1743149cc4e51
alirioinditex <aliriomam@ext.inditex.com>
2024-05-06 19:42:40 +0200

    first commit

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Alirio Aranguren on 31/1/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
